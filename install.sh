e5647204c25f3d0c3d32166fdc132b9af9f98fb5
Jari Aalto <jari.aalto@cante.net>
2025-02-07 13:51:02 +0200

    install.sh: correct POSIX and shellcheck

--- install.sh
+++ install.sh
@@ -18,28 +18,35 @@
 #
 #       You should have received a copy of the GNU General Public License
 #       along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
+
+PROGRAM=$0
 
 Help ()
 {
     echo "\
-Synopsis: $0 <install option>
+Synopsis: $PROGRAM {--install|--remove} [--home|--local|--dest DIR]
 
 OPTIONS
-  -d, --dest DIR
-     Select DIR as destination
+    -d, --dest DIR
+        Select DIR as destination
 
-  -H, --home
-     Select ~/bin
+    -H, --home
+        Select ~/bin
 
-  -i, --install
-     Install
+    -i, --install
+        Install
 
-  -l, --local
-     Select /usr/local/bin
+    -l, --local
+        Select /usr/local/bin
 
-  -r, --remove
-     Remove install"
+    -r, --remove
+        Remove install
+
+DESCRIPTION
+
+    Install or remove lint.sh and javalint symlink."
+
+    exit 0
 }
 
 Warn ()
@@ -53,26 +60,35 @@ Die ()
     exit 1
 }
 
-Install ()
-{
-    ${test:+echo} ln --force --symbolic --relative lint.sh $1
-    (${test:+echo} cd $1 && ${test:+echo} ln -s lint.sh javalint)
-}
+Install ()  # Run in a subshell
+{(
+    ${test:+echo} ln --force --symbolic --relative lint.sh "$1"
+
+    ${test:+echo} cd "$1" &&
+    ${test:+echo} ln -s lint.sh javalint
+)}
 
 Remove ()
 {
-    ${test:+echo} rm --force $1/lint.sh $1/javalint
+    ${test:+echo} rm --force "$1/lint.sh" "$1/javalint"
 }
 
 Main ()
 {
+    local dummy dest remove install
+
     while :
     do
+        # unused
+        # shellcheck disable=SC2034
+        dummy="OPT: $1"
+
         case "$1" in
-            -d | --dest)
-                [ "$2" ] || Die "ERROR: Missing dir"
+            -d | --dest | --dir)
                 dest=$2
-                shift 2
+                [ "$dest" ] || Die "ERROR: Missing --dest DIR"
+                [ -d "$dest" ] || Die "ERROR: no dir: $dest"
+                shift ; shift
                 ;;
             -H | --home)
                 dest=$HOME/bin
@@ -92,11 +108,14 @@ Main ()
                 ;;
             -h | --help)
                 Help
-                return 0
                 ;;
             --)
                 shift
                 break
+                ;;
+            -*)
+                Warn "WARN: unknown option: $1"
+                shift
                 ;;
             *)
                 break
@@ -105,7 +124,7 @@ Main ()
     done
 
     if [ ! "$install" ] && [ ! "$remove" ] ; then
-        echo "Install utility. See --help"
+        echo "ERROR: Missing option --install or --remove. See --help"
         return 1
     fi
 
@@ -118,8 +137,8 @@ Main ()
         Die "ERROR: Not a directory: $dest"
     fi
 
-    [ "$install" ] && Install $dest
-    [ "$remove"  ] && Remove  $dest
+    [ "$install" ] && Install "$dest"
+    [ "$remove"  ] && Remove  "$dest"
 }
 
 Main "$@"
